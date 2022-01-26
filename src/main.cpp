4402781999f84f1c28a0abb02e8f448d741053a1
jannikac <jannikac@mailbox.org>
2022-01-26 18:42:29 +0100

    Cleanup

--- src/main.cpp
+++ src/main.cpp
@@ -2,27 +2,12 @@
 #include "string.hpp"
 #include <iostream>
 
-int b() {
-  string s("hallo ");
-  char *data = s.data();
-  s.append("Welt!");
-  data[0] = 'H';
-  s.print();
-  return 0;
-}
-
-void print_string(string s) { s.print(); }
-
 int main() {
-  // std::cout << "hello world" << std::endl;
+  // Test code:
   // string s2("asd");
   // s2.append("fgh");
   // s2.clear();
   // s2.append("xyz");
   // s2.print();
-  string test("test");
-  print_string(test);
-  test.print();
-
-  // b();
+  return 0;
 }
